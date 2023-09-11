eb11475b7ba5a1e55017180b1eec350484cfb6f5
hytch <naofea9@gmail.com>
2023-09-11 20:37:46 +0900

    FormulaStructのコントローラを追加

---
+++ Controllers/FormulaStructController.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MathApi.Models;
+
+namespace MathApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class FormulaStructController : ControllerBase
+  {
+    private readonly MathDbContext _context;
+
+    public FormulaStructController(MathDbContext context)
+    {
+      _context = context;
+    }
+
+    // GET: api/FormulaStruct
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<FormulaStruct>>> GetFormulaStructs()
+    {
+      if (_context.FormulaStructs == null)
+      {
+        return NotFound();
+      }
+      return await _context.FormulaStructs.ToListAsync();
+    }
+
+    // GET: api/FormulaStruct/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<FormulaStruct>> GetFormulaStruct(long id)
+    {
+      if (_context.FormulaStructs == null)
+      {
+        return NotFound();
+      }
+      var formulaStruct = await _context.FormulaStructs.FindAsync(id);
+
+      if (formulaStruct == null)
+      {
+        return NotFound();
+      }
+
+      return formulaStruct;
+    }
+
+    // PUT: api/FormulaStruct/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutFormulaStruct(long id, FormulaStruct formulaStruct)
+    {
+      if (id != formulaStruct.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(formulaStruct).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!FormulaStructExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/FormulaStruct
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<FormulaStruct>> PostFormulaStruct(FormulaStructDto dto)
+    {
+      if (_context.FormulaStructs == null)
+      {
+        return Problem("Entity set 'MathDbContext.FormulaStructs'  is null.");
+      }
+      var formulaStruct = dto.CreateModel();
+      var symbols = await _context.Symbols.Where(s => formulaStruct.Strings.Select(st => st.SymbolId).Contains(s.Id)).ToListAsync();
+      var labels = await _context.FormulaLabels.Where(l => formulaStruct.Arguments.Select(a => a.LabelId).Contains(l.Id)).ToListAsync();
+
+      var tmpStructStrings = new List<FormulaStructString>();
+      for (var i = formulaStruct.Strings.Count; i >= 0; i--)
+      {
+        var str = formulaStruct.Strings[i];
+        if (!str.SymbolId.HasValue)
+        {
+          var arg = formulaStruct.Arguments.Find(a => a.SerialNo == str.ArgumentSerialNo);
+          if (arg == null)
+            return BadRequest($"ArgumentSerialNo mismatch (StringsSerialNo={str.SerialNo})");
+          var label = labels.Find(l => l.Id == arg.LabelId);
+          if (label == null)
+            return BadRequest($"LabelId#{arg.LabelId} is not found (StringsSerialNo={str.SerialNo})");
+          if (str.Substitutions.Count > 0)
+          {
+            if (label.TypeId != Const.FormulaLabelTypeEnum.Proposition)
+              return BadRequest($"Only Proposition Label can have Substitutions (StringsSerialNo={str.SerialNo})");
+          }
+
+          foreach (var ss in str.Substitutions)
+          {
+            var argFr = formulaStruct.Arguments.Find(a => a.SerialNo == ss.ArgumentFromSerialNo);
+            if (argFr == null)
+              return BadRequest($"Substitution ArgumentFromSerialNo mismatch (StringsSerialNo={str.SerialNo})");
+            var labelFr = labels.Find(l => l.Id == argFr.LabelId);
+            if (labelFr == null)
+              return BadRequest($"(Substitution) LabelId#{arg.LabelId} is not found (StringsSerialNo={str.SerialNo})");
+            if (labelFr.TypeId != Const.FormulaLabelTypeEnum.FreeVariable)
+              return BadRequest("Substitution From must be FreeVariable (StringsSerialNo={str.SerialNo})");
+            var argTo = formulaStruct.Arguments.Find(a => a.SerialNo == ss.ArgumentToSerialNo);
+            if (argTo == null)
+              return BadRequest($"Substitution ArgumentToSerialNo mismatch (StringsSerialNo={str.SerialNo})");
+            var labelTo = labels.Find(l => l.Id == argTo.LabelId);
+            if (labelTo == null)
+              return BadRequest($"(Substitution) LabelId#{arg.LabelId} is not found (StringsSerialNo={str.SerialNo})");
+            if (labelTo.TypeId != Const.FormulaLabelTypeEnum.FreeVariable && labelTo.TypeId != Const.FormulaLabelTypeEnum.Term)
+              return BadRequest("Substitution From must be FreeVariable or Term (StringsSerialNo={str.SerialNo})");
+          }
+
+          tmpStructStrings.Add(formulaStruct.Strings[i]);
+        }
+        else
+        {
+          var symbol = symbols.Find(s => s.Id == str.SymbolId);
+          if (symbol == null)
+            return BadRequest($"SymbolId#{str.SymbolId} (StringsSerialNo={str.SerialNo}) is not found");
+
+          if (symbol.Arity != tmpStructStrings.Count)
+            return BadRequest($"argument count of symbol#{symbol.Id} (StringsSerialNo={str.SerialNo}) is invalid");
+
+          if (symbol.IsQuantifier)
+          {
+            if (!str.BoundArgumentSerialNo.HasValue)
+              return BadRequest($"Quantifier must have a BoundArgument (StringsSerialNo={str.SerialNo})");
+
+            var boundVarArg = formulaStruct.Arguments.Find(a => a.SerialNo == str.BoundArgumentSerialNo);
+            if (boundVarArg == null)
+              return BadRequest($"ArgumentSerialNo is mismatch (StringsSerialNo={str.SerialNo})");
+
+            var boundLabel = labels.Find(l => l.Id == boundVarArg.LabelId);
+            if (boundLabel == null)
+              return BadRequest($"FormulaLabel#{boundVarArg.LabelId} is not found (StringsSerialNo={str.SerialNo})");
+
+            if (boundLabel.TypeId != Const.FormulaLabelTypeEnum.FreeVariable)
+              return BadRequest($"BoundVariable must be FreeVariable (StringsSerialNo={str.SerialNo})");
+          }
+
+          foreach (var tmpFss in tmpStructStrings)
+          {
+            var arg = formulaStruct.Arguments.Find(a => a.SerialNo == tmpFss.ArgumentSerialNo);
+            if (arg == null)
+              return BadRequest($"ArgumentSerialNo mismatch (StringsSerialNo={str.SerialNo})");
+            var label = labels.Find(l => l.Id == arg.LabelId);
+            if (label == null)
+              return BadRequest($"LabelId#{arg.LabelId} is not found (StringsSerialNo={str.SerialNo})");
+            if (symbol.ArityFormulaTypeId == Const.FormulaTypeEnum.Term)
+            {
+              if (label.TypeId != Const.FormulaLabelTypeEnum.Term)
+                return BadRequest($"ArityType mismatch (StringsSerialNo={str.SerialNo})");
+            }
+            if (symbol.ArityFormulaTypeId == Const.FormulaTypeEnum.Proposition)
+            {
+              if (label.TypeId != Const.FormulaLabelTypeEnum.Proposition)
+                return BadRequest($"ArityType mismatch (StringsSerialNo={str.SerialNo})");
+            }
+          }
+        }
+      }
+
+      _context.FormulaStructs.Add(formulaStruct);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetFormulaStruct", new { id = formulaStruct.Id }, formulaStruct);
+    }
+
+    // DELETE: api/FormulaStruct/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteFormulaStruct(long id)
+    {
+      if (_context.FormulaStructs == null)
+      {
+        return NotFound();
+      }
+      var formulaStruct = await _context.FormulaStructs.FindAsync(id);
+      if (formulaStruct == null)
+      {
+        return NotFound();
+      }
+
+      _context.FormulaStructs.Remove(formulaStruct);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool FormulaStructExists(long id)
+    {
+      return (_context.FormulaStructs?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+  }
+}
---
+++ Controllers/FormulaStructDto.cs
@@ -0,0 +1,63 @@
+using MathApi.Models;
+
+namespace MathApi.Controllers;
+
+public class FormulaStructDto
+{
+  public long Id { get; set; }
+  public string? Meaning { get; set; }
+  public List<FormulaStructArgumentDto> Arguments { get; set; } = new();
+  public List<FormulaStructStringDto> Strings { get; set; } = new();
+
+  public class FormulaStructArgumentDto
+  {
+    public int SerialNo { get; set; }
+    public int LabelId { get; set; }
+  }
+  public class FormulaStructStringDto
+  {
+    public int SerialNo { get; set; }
+    public long? SymbolId { get; set; }
+    public int? BoundArgumentSerialNo { get; set; }
+    public int? ArgumentSerialNo { get; set; }
+    public List<FormulaStructStringSubstitutionDto> Substitutions { get; set; } = new();
+
+    public class FormulaStructStringSubstitutionDto
+    {
+      public int SerialNo { get; set; }
+      public int ArgumentFromSerialNo { get; set; }
+      public int ArgumentToSerialNo { get; set; }
+    }
+  }
+
+  public FormulaStruct CreateModel()
+  {
+    return new FormulaStruct
+    {
+      Id = Id,
+      Meaning = Meaning,
+      Arguments = Arguments.OrderBy(a => a.SerialNo).Select(a => new FormulaStructArgument
+      {
+        FormulaStructId = Id,
+        SerialNo = a.SerialNo,
+        LabelId = a.LabelId
+      }).ToList(),
+      Strings = Strings.OrderBy(s => s.SerialNo).Select(s => new FormulaStructString
+      {
+        FormulaStructId = Id,
+        SerialNo = s.SerialNo,
+        SymbolId = s.SymbolId,
+        BoundArgumentSerialNo = s.BoundArgumentSerialNo,
+        ArgumentSerialNo = s.ArgumentSerialNo,
+        Substitutions = s.Substitutions.OrderBy(ss => ss.SerialNo).Select(sb => new FormulaStructStringSubstitution
+        {
+          FormulaStructId = Id,
+          FormulaStructStringSerialNo = s.SerialNo,
+          SerialNo = sb.SerialNo,
+          ArgumentFromSerialNo = sb.ArgumentFromSerialNo,
+          ArgumentToSerialNo = sb.ArgumentToSerialNo
+        }).ToList()
+      }).ToList()
+    };
+  }
+}
