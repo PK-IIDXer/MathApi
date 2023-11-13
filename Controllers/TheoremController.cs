abd0e860312eee18cdeebbf4b7454bf1fa229688
hytch <naofea9@gmail.com>
2023-11-13 15:25:08 +0900

    Theorem FormulaStruct版　InferenceArgumentMappingを作成するよう修正

--- Controllers/TheoremController.cs
+++ Controllers/TheoremController.cs
@@ -53,6 +53,8 @@ namespace MathApi.Controllers
         var labels = await _context.FormulaLabels.Where(fl => labelIds.Contains(fl.Id)).ToListAsync();
         if (!labels.Any(ia => ia.TypeId == Const.FormulaLabelTypeEnum.Proposition))
           throw new ArgumentException("Should not use inference if there is no propositional argument");
+
+        await SetMappingSerialNo(theorem.Inference);
       }
       _context.Theorems.Add(theorem);
       await _context.SaveChangesAsync();
@@ -74,6 +76,85 @@ namespace MathApi.Controllers
       await _context.SaveChangesAsync();
 
       return NoContent();
+    }
+
+    private async Task SetMappingSerialNo(Inference inference)
+    {
+      int serialNo = 0;
+      var mappings = new List<InferenceFormulaStructArgumentMapping>();
+      var tmpLabelIds = new List<int>();
+
+      // 仮定
+      foreach (var assumption in inference.Assumptions)
+      {
+        var asmFormulaStructArgs = await _context.FormulaStructArguments
+                                              .Where(
+                                                fsa => fsa.FormulaStructId == assumption.FormulaStructId
+                                              ).ToListAsync();
+        foreach (var fsa in asmFormulaStructArgs)
+        {
+          var infArgs = inference.Arguments.Where(a => a.FormulaLabelId == fsa.LabelId);
+          if (infArgs.Count() != 1)
+            throw new ArgumentException("invalid FormulaLabelId in inference.Arguments");
+          var infArg = infArgs.First();
+          mappings.Add(new InferenceFormulaStructArgumentMapping
+          {
+            SerialNo = serialNo,
+            FormulaStructId = fsa.FormulaStructId,
+            FormulaStructArgumentSerialNo = fsa.SerialNo,
+            InferenceArgumentSerialNo = infArg.SerialNo
+          });
+        }
+        assumption.FormulaStructArgumentMappingSerialNo = serialNo++;
+
+        // 解消可能仮定
+        if (assumption.DissolutableAssumption == null)
+          continue;
+        var disFormulaStructArgs = await _context.FormulaStructArguments
+                                                 .Where(
+                                                   fsa => fsa.FormulaStructId == assumption.DissolutableAssumption.FormulaStructId
+                                                 ).ToListAsync();
+        foreach (var fsa in disFormulaStructArgs)
+        {
+          var infArgs = inference.Arguments.Where(a => a.FormulaLabelId == fsa.LabelId);
+          if (infArgs.Count() != 1)
+            throw new ArgumentException("invalid FormulaLabelId in inference.Arguments");
+          var infArg = infArgs.First();
+          mappings.Add(new InferenceFormulaStructArgumentMapping
+          {
+            SerialNo = serialNo,
+            FormulaStructId = fsa.FormulaStructId,
+            FormulaStructArgumentSerialNo = fsa.SerialNo,
+            InferenceArgumentSerialNo = infArg.SerialNo
+          });
+        }
+        assumption.DissolutableAssumption.FormulaStructArgumentMappingSerialNo = serialNo++;
+      }
+
+      // 結論
+      var formulaStructArgs = await _context
+        .FormulaStructArguments
+        .IgnoreAutoIncludes()
+        .Where(
+          fsa => fsa.FormulaStructId == inference.Conclusion.FormulaStructId
+        ).ToListAsync();
+      foreach (var fsa in formulaStructArgs)
+      {
+        var infArgs = inference.Arguments.Where(a => a.FormulaLabelId == fsa.LabelId);
+        if (infArgs.Count() != 1)
+          throw new ArgumentException("invalid FormulaLabelId in inference.Arguments");
+        var infArg = infArgs.First();
+        mappings.Add(new InferenceFormulaStructArgumentMapping
+        {
+          SerialNo = serialNo,
+          FormulaStructId = fsa.FormulaStructId,
+          FormulaStructArgumentSerialNo = fsa.SerialNo,
+          InferenceArgumentSerialNo = infArg.SerialNo
+        });
+      }
+      inference.Conclusion.FormulaStructArgumentMappingSerialNo = serialNo++;
+
+      inference.FormulaStructArgumentMappings = mappings;
     }
   }
 }
